41dcb82047f0e70230b4b1d6b218454fea859106
kenny-kogi <mwangiken497@gmail.com>
2022-03-04 11:12:41 +0300

    styled therapist dash

--- src/components/Dashboards/Therapist/TherapistDash.js
+++ src/components/Dashboards/Therapist/TherapistDash.js
@@ -1,11 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../../Home/Navbar/Therapist/Navbar";
 import SideMenu from "../../Shared/Therapist/SideMenu";
-import { Flex, Container, Center, Text, Box, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Container,
+  Center,
+  Text,
+  Box,
+  Button,
+  Image,
+  Heading,
+  Badge,
+  IconButton,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { AppContext } from "../../../context/appcontext";
 import Spinner from "../../Shared/Spinner";
 import { useNavigate } from "react-router";
+import PatientSvg from "../../../assets/pt.png";
+import ProfileSvg from "../../../assets/user-profile-svgrepo-com.svg";
+import { MdDelete } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
 
 const TherapistDash = () => {
   const { therapist } = useContext(AppContext);
@@ -32,9 +47,25 @@ const TherapistDash = () => {
   return (
     <>
       <Navbar />
+
       <Flex flexDirection="row" pt="100px">
         <SideMenu />
         <Container maxWidth="7xl" pt={5} ml={300}>
+          <Heading
+            as="h1"
+            size="sm"
+            letterSpacing="wide"
+            fontWeight="bold"
+            color="purple"
+            fontSize="30px"
+            mb="30"
+            textAlign="center"
+          >
+            <Text as="span" color="pink.400" fontSize="40px">
+              Welcome Dr. {therapist.firstName},{" "}
+            </Text>
+            Here is Your Patient Dashboard
+          </Heading>
           <Flex flexDirection="row" wrap="wrap" gap={10}>
             {nullCheckerPatient ? (
               <Center>
@@ -52,10 +83,23 @@ const TherapistDash = () => {
                     boxShadow="xl"
                     borderWidth={2}
                   >
-                    <Flex flexDirection="column" justifyContent="space-between">
-                      {patient.username}
+                    <Flex flexDirection="column">
+                      <Badge textAlign="center" width={100} mb={2}>
+                        {" "}
+                        {patient.firstName}
+                      </Badge>
+                      <Image
+                        ml={14}
+                        src={PatientSvg}
+                        alt="Patient Svg"
+                        height={150}
+                        width={150}
+                        mb={4}
+                      />
                       <Button
-                        m={5}
+                        ml={5}
+                        mr={5}
+                        mb={2}
                         bg="pink.400"
                         color="white"
                         _hover={{ bg: "purple" }}
@@ -66,6 +110,32 @@ const TherapistDash = () => {
                       >
                         View Analysis
                       </Button>
+
+                      <Flex direction="row" justifyContent="space-evenly">
+                        <IconButton
+                          variant="outline"
+                          colorScheme="blue"
+                          aria-label="Call Sage"
+                          fontSize="20px"
+                          icon={<CgProfile />}
+                          width={100}
+                          onClick={() => {
+                            console.log("Profile");
+                          }}
+                        />
+
+                        <IconButton
+                          variant="outline"
+                          colorScheme="red"
+                          aria-label="Call Sage"
+                          fontSize="30px"
+                          width={100}
+                          icon={<MdDelete />}
+                          onClick={() => {
+                            console.log("delete");
+                          }}
+                        />
+                      </Flex>
                     </Flex>
                   </Box>
                 );
